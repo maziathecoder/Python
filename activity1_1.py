7611bd27272d02f0a0c12944058b3540d51cd69c
maziathecoder <bajiwa@gmail.com>
2025-09-21 12:35:02 +0500

    v1

---
+++ activity1_1.py
@@ -0,0 +1,9 @@
+#printing a greeting message
+print("Hello, World!")
+
+print("My Name Is Mazia Babar")
+
+print("I Love Playing SubwaySurfers")
+
+#performimg and printing a simple claculation
+print("the result of 3+5 is : ", 3+5)
