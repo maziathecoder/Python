734e214b650e0cd6bfc3f9973b06f5f8aed0aada
maziathecoder <bajiwa@gmail.com>
2025-09-27 13:56:29 +0500

    v1

---
+++ activity4_2(ACP).py
@@ -0,0 +1,10 @@
+Draco = "five june"
+Mattheo = "five january"
+Blaise = "first november"
+Pansy = "twenty second march"
+Theodore = "thirteen april"
+print(Draco)
+print(Mattheo)
+print(Blaise)
+print(Pansy)
+print(Theodore)
