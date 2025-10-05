2a7bc7d2e561f4dc122d0b101a83e3026c967063
maziathecoder <bajiwa@gmail.com>
2025-10-05 12:29:31 +0500

    v1

---
+++ activity3_4.py
@@ -0,0 +1,10 @@
+print("enter marks optain in 4 subjects")
+science = int(input("science: "))
+computer = int(input("computer: "))
+english = int(input("english: "))
+maths = int(input("maths: "))
+sum = science + computer + english + maths
+print("sum of science, computer, enlish and maths")
+perc = (sum/400)*100
+print(end="perc marks = ")
+print(perc)
