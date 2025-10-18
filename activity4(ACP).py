69987ac3c0cd8901cd682a05c33f5b4a192614a9
maziathecoder <bajiwa@gmail.com>
2025-10-18 12:17:49 +0500

    v1

---
+++ activity4(ACP).py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+
+if age >= 10:
+    if age <= 20:
+        print("You are allowed")
+    else:
+        print("You are not allowed")
+else:
+    print("You are not allowed")
