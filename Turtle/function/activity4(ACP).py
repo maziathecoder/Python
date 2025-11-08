2cb5344c3f85bb8097860ff203a0c6ea9cc704ba
maziathecoder <bajiwa@gmail.com>
2025-11-08 13:14:39 +0500

    v1

---
+++ Turtle/function/activity4(ACP).py
@@ -0,0 +1,5 @@
+def circumference(radius):
+    pi = 3.14159
+    return 2 * pi * radius
+r = float(input("Enter the radius of the circle: "))
+print("Circumference of the circle is:", circumference(r))
