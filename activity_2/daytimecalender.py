ca45c7011ca7398251d54d3f6cc73eddfe97fd23
maziathecoder <bajiwa@gmail.com>
2025-11-23 12:07:15 +0500

    v1

---
+++ activity_2/daytimecalender.py
@@ -0,0 +1,6 @@
+from datetime import date, time, datetime
+today = date.today()
+now = datetime.now()
+print("todays date is ",today)
+print("\ncurrent date and time is ",now)
+print("\ndate components",today.year, today.month, today.day)
