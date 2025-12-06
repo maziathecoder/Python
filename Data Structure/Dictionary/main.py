67cad52630adb0a3088dbe2bac5b2c6e0a9f7b9e
maziathecoder <bajiwa@gmail.com>
2025-12-06 11:14:12 +0500

    v1

--- Data Structure/Dictionary/main.py
+++ Data Structure/Dictionary/main.py
@@ -1,29 +1,11 @@
-student_data = {'id1': 
-   {'name': ['Sara'], 
-    'class': ['V'], 
-    'subject_integration': ['english, math, science']
-   },
- 'id2': 
-  {'name': ['David'], 
-    'class': ['V'], 
-    'subject_integration': ['english, math, science']
-   },
- 'id3': 
-    {'name': ['Sara'], 
-    'class': ['V'], 
-    'subject_integration': ['english, math, science']
-   },
- 'id4': 
-   {'name': ['Surya'], 
-    'class': ['V'], 
-    'subject_integration': ['english, math, science']
-   },
-}
-
-result = {}
-
-for key,value in student_data.items():
-    if value not in result.values():
-        result[key] = value
-
-print(result)
+test_dict = {'Codingal' : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'Coding' : 1}
+  
+# printing original dictionary
+print("The original dictionary : " +  str(test_dict))
+# Initialize value 
+K = 2
+res = 0
+for key in test_dict:
+    if test_dict[key] == K:
+        res = res + 1
+print("Frequency of K is : " + str(res))
