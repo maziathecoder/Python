3b87a2a7abc4ce156c5e682753751c77a53abc9f
maziathecoder <bajiwa@gmail.com>
2025-12-13 11:05:32 +0500

    v1

--- Advanced functions/main.py
+++ Advanced functions/main.py
@@ -1,14 +1,23 @@
-# Add two lists using map and lambda
-numbers1 = [1, 2, 3]
-numbers2 = [4, 5, 6]
-result = map(lambda x, y: x + y, numbers1, numbers2)
-print("Addition of two lists")
-print(list(result))
+# Zip elements of two lists
+s1 = {2, 3, 1}
+s2 = {'b', 'a', 'c'}
+s3 = list(zip(s1, s2))
+print(s3,"\n")
 
-#using map
-nums = [1, 2, 3, 4, 5]  
-def sq(n):    
-    return n*n  
-square = list(map(sq, nums))
-print("Square of numbers in list")
-print(square)
+
+# Zip elements of two lists
+# Print elements one by one, but elements of 2nd list will be in reverse order
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+
+for x, y in zip(list1, list2[::-1]):
+    print(x, y)
+
+
+# Zip into dictionary
+stocks = ['reliance', 'infosys', 'tcs']
+prices = [2175, 1127, 2750]
+
+new_dict = {stocks: prices for stocks,
+			prices in zip(stocks, prices)}
+print('\n{}'.format(new_dict))
