24ab329aeaa45e80a4f13d92c24575176a39b8a6
maziathecoder <bajiwa@gmail.com>
2025-12-21 12:18:57 +0500

    v1

--- oops/oops_part_one/main.py
+++ oops/oops_part_one/main.py
@@ -1,21 +1,20 @@
-# create class
-class IOString():
-
-	# constructor to set default value
+# Create Class
+class Employee:
+  
+    # Initializing 
     def __init__(self):
-        self.str1 = ""
-
-	# function to get input from user
-    def get_String(self):
-        self.str1 = input("Enter String : ")
-
-	# function to print the string in upper case
-    def print_String(self):
-        print("Result is :", self.str1.upper())
-
-# Object creation
-str1 = IOString()
-
-# Call functions
-str1.get_String()
-str1.print_String()
+        print('Employee created')
+  
+    # Calling destructor
+    def __del__(self):
+        print("Destructor called")
+  
+def Create_obj():
+    print('Making Object...')
+    obj = Employee()
+    print('function end...')
+    return obj
+  
+print('Calling Create_obj() function...')
+obj = Create_obj()
+print('Program End...')
