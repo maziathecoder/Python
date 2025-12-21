2e96e93756c206a5c4967a212bf44c8b49d9b91c
maziathecoder <bajiwa@gmail.com>
2025-12-21 12:29:58 +0500

    v1

--- oops/oops_part_one/main.py
+++ oops/oops_part_one/main.py
@@ -1,20 +1,16 @@
-# Create Class
-class Employee:
-  
-    # Initializing 
-    def __init__(self):
-        print('Employee created')
-  
-    # Calling destructor
-    def __del__(self):
-        print("Destructor called")
-  
-def Create_obj():
-    print('Making Object...')
-    obj = Employee()
-    print('function end...')
-    return obj
-  
-print('Calling Create_obj() function...')
-obj = Create_obj()
-print('Program End...')
+# create a class
+class pair_elements:
+	
+	def twoSum(self, nums, target):
+		# create an empty dictionary
+		lookup = {}
+
+		# Iterate through the tuple
+		for i, num in enumerate(nums):
+			if target - num in lookup:
+				return (lookup[target - num], i )
+			lookup[num] = i
+
+# take input of dum from the user
+value = int(input("Enter sum for which you want to make this search : "))
+print("index1=%d, index2=%d" % pair_elements().twoSum((10,20,30,40,50,60,70),value))
