3ec65f35a30863e5189fbbb3bcd05c91aa078bdd
maziathecoder <bajiwa@gmail.com>
2025-12-27 11:15:12 +0500

    v1

--- Inheritance/main.py
+++ Inheritance/main.py
@@ -1,29 +1,30 @@
-# Python code to demonstrate how parent constructors
-# are called.
-
 # parent class
-class Person( object ):	
+class Bird:
+    
+    def __init__(self):
+        print("Bird is ready")
 
-		# __init__ is known as the constructor		
-		def __init__(self, name, idnumber):
-				self.name = name
-				self.idnumber = idnumber
-		def display(self):
-				print(self.name)
-				print(self.idnumber)
+    def whoisThis(self):
+        print("Bird")
+
+    def swim(self):
+        print("Swim faster")
 
 # child class
-class Employee( Person ):		
-		def __init__(self, name, idnumber, salary, post):
-				self.salary = salary
-				self.post = post
+class Penguin(Bird):
 
-				# invoking the __init__ of the parent class
-				Person.__init__(self, name, idnumber)
+    def __init__(self):
+        # call super() function
+        super().__init__()
+        print("Penguin is ready")
 
-				
-# creation of an object variable or an instance
-a = Employee('Rahul', 886012, 200000, "Intern")	
+    def whoisThis(self):
+        print("Penguin")
 
-# calling a function of the class Person using its instance
-a.display()
+    def run(self):
+        print("Run faster")
+
+peggy = Penguin()
+peggy.whoisThis()
+peggy.swim()
+peggy.run()
