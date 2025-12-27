a29e9f549706929a0db2e9c176a2efd138fe525f
maziathecoder <bajiwa@gmail.com>
2025-12-27 12:34:57 +0500

    v1

--- Inheritance/activity4(ACP).py
+++ Inheritance/activity4(ACP).py
@@ -1,18 +1,12 @@
-class Vehicle:
+class vehicle:
     def __init__(self, capacity):
         self.capacity = capacity
-
     def fare(self):
         return self.capacity * 100
-
-
-class Bus(Vehicle):
+class bus(vehicle):
     def fare(self):
         base_fare = super().fare()
         maintenance_charge = base_fare * 0.10
         return base_fare + maintenance_charge
-
-
-# Example usage
-bus = Bus(50)
-print("Total Bus Fare: INR", bus.fare())
+bus = bus(50)
+print("total bus fare: INR", bus.fare())
