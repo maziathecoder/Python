713723ee40117d05cf34ebd33cf8c2a44d65136b
maziathecoder <bajiwa@gmail.com>
2025-12-28 12:25:05 +0500

    v1

--- encapsulation/main.py
+++ encapsulation/main.py
@@ -1,21 +1,13 @@
-class Computer:
+# Create class
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
 
-    def __init__(self):
-        self.__maxprice = 900
+	# Method to print points in coordinate format
+    def __str__(self):
+        return "({0}, {1})".format(self.x, self.y)
 
-    def sell(self):
-        print("Selling Price: {}".format(self.__maxprice))
-
-    def setMaxPrice(self, price):
-        self.__maxprice = price
-
-c = Computer()
-c.sell()
-
-# change the price
-c.__maxprice = 1000
-c.sell()
-
-# using setter function
-c.setMaxPrice(1000)
-c.sell()
+# Create Object
+p1 = Point(2, 3)
+print(p1)
