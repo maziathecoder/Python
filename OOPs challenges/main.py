6b0f51003fbe048270485580066a0278cf39191d
maziathecoder <bajiwa@gmail.com>
2026-01-10 11:20:31 +0500

    v1

--- OOPs challenges/main.py
+++ OOPs challenges/main.py
@@ -1,34 +1,27 @@
-import random
+# Python program to overload equality
+# and less than operators
 
-class FruitQuiz:
-
-	# Create a constructor
-	def __init__(self):
-		
-		# Create a dictionary of fruits as keys and color as value
-		self.fruits={'apple':'red',
-					'orange':'orange',
-					'watermelon':'green',
-					'banana':'yellow'}
-
-	# Function for the quiz, here a fruit will be chosen randomly	
-	def quiz(self):
-		while (True):
-			
-			fruit, color = random.choice(list(self.fruits.items()))
-			
-			print("What is the color of {}".format(fruit))
-			user_answer = input()
-			
-			if(user_answer.lower() == color):
-				print("Correct answer")
-			else:
-				print("Wrong answer")
+class A:
+	def __init__(self, a):
+		self.a = a
+	def __lt__(self, other):
+		if(self.a<other.a):
+			return "ob1 is less than ob2"
+		else:
+			return "ob2 is less than ob1"
+	def __eq__(self, other):
+		if(self.a == other.a):
+			return "Both are equal"
+		else:
+			return "Not equal"
 				
-			option = int(input("enter 0 , if you want to play again otherwise enter 1: "))
-			if (option):
-				break
+ob1 = A(2)
+ob2 = A(3)
+print("Passed Values :", ob1.a, ob2.a)
+print(ob1 < ob2)
 
-print("welcome to fruit quiz ")
-fq = FruitQuiz()
-fq.quiz()
+ob3 = A(4)
+ob4 = A(4)
+print("Passed Values :", ob3.a, ob4.a)
+print(ob3 == ob4)
+
